42954caad2bd486d78b7d100d27cc58fa9a72e5d
Kyle Southworth <kyle.southworth@kainos.com>
2025-02-12 13:23:01 +0000

    updated patch type

--- CloudPlatform/AWS/iam_data.tf
+++ CloudPlatform/AWS/iam_data.tf
@@ -113,6 +113,8 @@ data "aws_iam_policy_document" "aws-ebs-csi-driver_policy" {
     sid    = "Ec2Access"
     effect = "Allow"
     actions = [
+      "ec2:CreateVolume",
+      "ec2:DeleteVolume",
       "ec2:ModifyVolume",
       "ec2:DetachVolume",
       "ec2:DescribeVolumes",
@@ -122,7 +124,8 @@ data "aws_iam_policy_document" "aws-ebs-csi-driver_policy" {
       "ec2:DescribeInstances",
       "ec2:DescribeAvailabilityZones",
       "ec2:CreateSnapshot",
-      "ec2:AttachVolume"
+      "ec2:AttachVolume",
+      "ec2:CreateTags"
     ]
     resources = ["*"]
   }
