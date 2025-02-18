6606d237f93224b059df4fbdd0dcc5db2ff41ecc
Nick Moynihan <nick.moynihan@kainos.com>
2025-02-18 13:19:18 +0000

    re-add helm and k8s config

--- CloudPlatform/AWS/provider.tf
+++ CloudPlatform/AWS/provider.tf
@@ -16,10 +16,10 @@ terraform {
     #   source  = "hashicorp/kubernetes"
     #   version = "~> 2.35"
     # }
-    # helm = {
-    #   source  = "hashicorp/helm"
-    #   version = "~> 2.17"
-    # }
+     helm = {
+       source  = "hashicorp/helm"
+       version = "~> 2.17"
+     }
   }
 }
 
@@ -33,12 +33,12 @@ provider "aws" {
   }
 }
 
-# provider "kubernetes" {
-#   config_path = "~/.kube/config"
-# }
+ provider "kubernetes" {
+   config_path = "~/.kube/config"
+ }
 
-# provider "helm" {
-#   kubernetes {
-#     config_path = "~/.kube/config"
-#   }
-# }
+ provider "helm" {
+   kubernetes {
+     config_path = "~/.kube/config"
+   }
+ }
